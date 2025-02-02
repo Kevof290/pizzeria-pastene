e9c7b290199cc0b7b9eeafb04e753f98b48b9819
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-02-02 17:07:44 -0300

    subiendo repo

---
+++ src/assets/components/CardPizza.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const CardPizza = ({name, price, ingredients, img}) => {
+  return (
+    <>
+    <img className="image rounded-top" src={img} alt={name} />
+    <h3 className="text-start mt-2 mx-2">Pizza {name}</h3>
+    <h3 className="fs-4 fw-normal">Ingredientes</h3>
+    <p>🍕 {ingredients.join(', ')}</p>
+    <h2 className="mb-4">Precio: {price}</h2>
+    <div className="mb-3">
+    <button className="btn btn-outline-dark mx-2">Ver más 👀</button>
+    <button className="btn btn-dark mx-2">Añadir 🛒</button>
+    </div>
+    </>
+  )
+}
+
+CardPizza.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+  img: PropTypes.string.isRequired,
+};
+
+export default CardPizza
---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light text-center p-3">
+        <p>© 2025 - Pizzeria Pastene - Todos los derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer
