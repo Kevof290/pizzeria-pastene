a3ffe9907065ae4c50a75e46de7624afae5134d2
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-02-09 16:28:16 -0300

    add: code order

--- src/assets/components/CardPizza.jsx
+++ src/assets/components/CardPizza.jsx
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-const CardPizza = ({name, price, ingredients, img}) => {
+const CardPizza = ({ name, price, ingredients, img }) => {
   return (
     <>
-    <img className="image rounded-top" src={img} alt={name} />
-    <h3 className="text-start mt-2 mx-2">Pizza {name}</h3>
-    <h3 className="fs-4 fw-normal">Ingredientes</h3>
-    <p>🍕 {ingredients.join(', ')}</p>
-    <h2 className="mb-4">Precio: {price}</h2>
-    <div className="mb-3">
-    <button className="btn btn-outline-dark mx-2">Ver más 👀</button>
-    <button className="btn btn-dark mx-2">Añadir 🛒</button>
-    </div>
+      <img className='image rounded-top' src={img} alt={name} />
+      <h3 className='text-start mt-2 mx-2'>Pizza {name}</h3>
+      <h3 className='fs-4 fw-normal'>Ingredientes</h3>
+      <p>🍕 {ingredients.join(', ')}</p>
+      <h2 className='mb-4'>Precio: {price}</h2>
+      <div className='mb-3'>
+        <button className='btn btn-outline-dark mx-2'>Ver más 👀</button>
+        <button className='btn btn-dark mx-2'>Añadir 🛒</button>
+      </div>
     </>
   )
 }
@@ -20,7 +20,7 @@ CardPizza.propTypes = {
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
-  img: PropTypes.string.isRequired,
-};
+  img: PropTypes.string.isRequired
+}
 
 export default CardPizza
