a0f00645e609295dfbc5c1ecd0fa677b7bc809c0
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-02-09 17:56:08 -0300

    fix: footer position

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='bg-dark text-light text-center p-3'>
+    <footer className='bg-dark text-light text-center p-3 position-fixed bottom-0 w-100'>
       <p>© 2025 - Pizzeria Pastene - Todos los derechos reservados</p>
     </footer>
   )
